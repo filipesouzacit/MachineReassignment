e82dad44ad338e8bc98d66579f1086478e0677b4
filipe <filipe.luca-desouza@mycit.ie>
2021-07-27 12:45:18 +0100

    correct bug on domain creating 2

--- readyToRun.bat
+++ readyToRun.bat
@@ -2,4 +2,4 @@
 g++ -std=c++14  -o MachineReassignment MachineReassignment.cpp
 chmod a+x MachineReassignment
 chmod a+x script.run
-nohup ./script.run > ../result/data/out24-07.log &
+nohup ./script.run > ../result/data/outT1.log &
