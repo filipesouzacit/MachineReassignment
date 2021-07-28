53dac13828be50cc308de03f1c36daed96e7040f
filipe <filipe.luca-desouza@mycit.ie>
2021-07-29 00:45:45 +0100

    bug on random seed2

--- readyToRun.bat
+++ readyToRun.bat
@@ -4,5 +4,7 @@ chmod a+x MachineReassignment
 chmod a+x script.run
 nohup ./script1.run > ../result/data/out27-06-S1.log &
 
+git stash push --include-untracked
+git stash drop
 
 ./MachineReassignment typeSearch=2 instance=a2_1 test=007 selectProcesses=4 subProblemSizeInit=10 subProblemSizeMax=10 improvementThreshold=9999999 runTime=10 failuresMax=400 discrepancyMax=5 seed=1 printFreq=1
