22dd4069dbf83278442d595b40b89b6ef3e03f68
filipe <filipe.luca-desouza@mycit.ie>
2021-08-23 15:59:16 +0100

    =23/08/2021

--- readyToRun.bat
+++ readyToRun.bat
@@ -1,8 +1,16 @@
 #!/bin/bash
 g++ -std=c++14  -o MachineReassignment MachineReassignment.cpp
 chmod a+x MachineReassignment
-chmod a+x script.run
-nohup ./script1.run > ../result/data/out21-08-S1.log &
+chmod a+x script1.run
+chmod a+x script2.run
+chmod a+x script3.run
+chmod a+x script4.run
+chmod a+x script5.run
+nohup ./script1.run > ../result/data/out210822-S1.log &
+nohup ./script2.run > ../result/data/out210822-S2.log &
+nohup ./script3.run > ../result/data/out210822-S3.log &
+nohup ./script4.run > ../result/data/out210822-S4.log &
+nohup ./script5.run > ../result/data/out210822-S5.log &
 
 ssh fdesouza@143.239.81.1 -p 40022
 ssh fdesouza@143.239.81.140
